b3521446618d773717d8875209cf04a46fa1d29e
cocoshe <69197635+cocoshe@users.noreply.github.com>
2022-01-08 15:05:47 +0800

    Update user.go

--- models/user.go
+++ models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	Id    int    `json:"id" from:"id" primaryKey:"true"`
 	Pwd   string `json:"pwd" from:"pwd" primaryKey:"required"`
 	Name  string `json:"name" from:"name" primaryKey:"required"`
-	Level int    `json:"age" from:"age" primaryKey:"required"`
+	Level int    `json:"level" from:"level" primaryKey:"required"`
 }
