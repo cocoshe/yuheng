dd561c8e53a84e143cf1ee49b892e3a7bfc2b091
cocoshe <1228759711@qq.com>
2022-01-14 20:52:48 +0800

    simplize model

--- models/user.go
+++ models/user.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	Id    string `json:"id" from:"id" primaryKey:"true"`
-	Pwd   string `json:"pwd" from:"pwd" primaryKey:"required"`
-	Name  string `json:"name" from:"name" primaryKey:"required"`
-	Level int    `json:"level" from:"level" primaryKey:"required"`
+	Id    string `json:"id" primaryKey:"true"`
+	Pwd   string `json:"pwd"`
+	Name  string `json:"name"`
+	Level int    `json:"level"`
 }
 
 //func (User) TableName() string {
