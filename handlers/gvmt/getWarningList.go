447df537e3f23c8cffc13d11913536895499568f
cocoshe <1228759711@qq.com>
2022-01-14 21:34:40 +0800

    update model type

---
+++ handlers/gvmt/getWarningList.go
@@ -0,0 +1,8 @@
+package gvmt
+
+import "github.com/gin-gonic/gin"
+
+// TODO: 获取可能违法的企业的相关信息，发送给前端展示
+func GetWarningList(c *gin.Context) {
+
+}
--- models/user.go
+++ models/user.go
@@ -1,11 +1,21 @@
 package models
 
 type User struct {
-	Id    string `json:"id" primaryKey:"true"`
-	Pwd   string `json:"pwd"`
-	Name  string `json:"name"`
-	Level int    `json:"level"`
+	Id    string `json:"id" primaryKey:"true"` // 账号
+	Pwd   string `json:"pwd"`                  // 密码
+	Name  string `json:"name"`                 // 昵称/姓名
+	Level int    `json:"level"`                // 权限
 }
+
+/*
+example:
+{
+    "id": "20041329",
+    "pwd": "123",
+    "name": "cocoshe",
+    "level": 100
+}
+*/
 
 //func (User) TableName() string {
 //	return "user"
